94f71b7232a3329f196b64fdfc0d267428619408
zinlaess <fedorkovdima@gmail.com>
2023-04-14 12:07:29 +0300

    DF

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-let btn = document.querySelectorAll('.button')
+// let btn = document.querySelectorAll('.button')
+//
+//
+//  btn.forEach(function (item){
+//
+//     item.onclick = function (event){
+//         event.preventDefault();
+//         document.querySelector('.popup').classList.add('open')
+//     }
+// })
+// let closeModal = document.querySelector('.close_btn')
+//
+// closeModal.onclick = function (){
+//     document.querySelector('.popup.open').classList.remove('open')
+// }
 
+document.addEventListener('click', function (event){
 
- btn.forEach(function (item){
+    if(event.target.classList.contains('modal_open')) {
+        if (document.querySelector('#' + event.target.getAttribute('data-modal'))){
+            document.querySelector('#' + event.target.getAttribute('data-modal')).classList.add('open')
+        }
 
-    item.innerText += ' JS'
+    }
+    if (event.target.classList.contains('close_btn')) {
+        event.target.closest('.open').classList.remove('open')
+    }
 })
